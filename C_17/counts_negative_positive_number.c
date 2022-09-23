1da47cdb0bbe7758dfe65c5c13e803686ed012dc
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-09-23 19:50:47 +0200

    fix typo

--- C_17/counts_negative_positive_number.c
+++ C_17/counts_negative_positive_number.c
@@ -8,10 +8,9 @@ int main(){
     printf("Perfavore inserisci 5 numeri => \n");
 
     for(int i = 0; i < 5; i++){
-        printf("inserisci il primo numero:");
+        printf("inserisci il numero:");
         scanf("%d", &input);
         arr[i]=input;
-        printf("%d\n", arr[i]);
     }
 
     for(int ocaz = 0; ocaz < 5; ocaz++){

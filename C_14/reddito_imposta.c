60a75b701b7c931c89106265729944a866f32019
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-09-23 19:16:00 +0200

    mini fix su virgola mobile

--- C_14/reddito_imposta.c
+++ C_14/reddito_imposta.c
@@ -13,19 +13,19 @@ int main(){
         printf("1 del reddito \n");
     }
 
-    else if(reddito > SOGLIA_MINIMA && reddito <= 2.250f) {
+    else if(reddito > SOGLIA_MINIMA && reddito <= 2250.00f) {
         printf("7.50$ pi+ il 2 della quota sopra i 750$ \n");
      }
  
-    else if(reddito > 2.250f  && reddito <= 3.750f) {
+    else if(reddito > 2250.00f  && reddito <= 3750.00f) {
         printf("37.50$ pi+ il 3 della quota sopra i 2.250$ \n");
      }
 
-    else if(reddito > 3.750f  && reddito <= 5.250f) {
+    else if(reddito > 3750.00f  && reddito <= 5250.00f) {
         printf("82.50$ pi+ il 4 della quota sopra i 3.750$ \n");
      }
      
-    else if(reddito > 5.250f  && reddito <= 7.000f) {
+    else if(reddito > 5250.00f  && reddito <= 7000.00f) {
         printf("142.50$ pi+ il 5 della quota sopra i 5.250f$ \n");
      }
 

9cef202a841d01ace2e0b54a740cf85211b665e6
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-09-23 20:05:23 +0200

    gang

--- C_17/counts_negative_positive_number.c
+++ C_17/counts_negative_positive_number.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
+// Write a C program that read 5 numbers and counts the number of positive numbers and negative numbers
 
 int main(){
 
     int arr [5];
-    int input = 0;
+    int input = 0, positive = 0, negative =0;
     printf("Perfavore inserisci 5 numeri => \n");
 
     for(int i = 0; i < 5; i++){
         printf("inserisci il numero:");
         scanf("%d", &input);
         arr[i]=input;
+        arr[i] < 0 ? positive++ : negative++;
     }
 
     for(int ocaz = 0; ocaz < 5; ocaz++){
-        printf("%d\n", arr[ocaz]);
+        printf("%d ", arr[ocaz]);
     }
+
+    positive > negative ? 
+        printf("\nla maggioranza dei numeri presenti è quella dei positivi %d > %d", positive, negative):
+        printf("\nla maggioranza dei numeri presenti è quella dei negativi %d > %d", negative, positive);
     
     return 0; 
 }
